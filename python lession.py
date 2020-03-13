c1b3fa4d5bdae81b8a3556705f2c55d00b921982
Bala sundara rao Padavala <balasundararaopadavala@Balas-MacBook-Pro.local>
2020-03-13 16:39:22 -0700

    addition code

---
+++ python lession.py
@@ -0,0 +1,8 @@
+pasta = 16.68
+sauce = 6.98
+garlic = 16.78
+seasoning = 15.26
+bread = 3.00
+meatballs = 4.395
+subtotal = round ((pasta + sauce + garlic + seasoning + bread + meatballs), 3)
+print(subtotal)
